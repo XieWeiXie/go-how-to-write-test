e49557d3179d80140ac13ebe033dccae92f9ad68
XieWei <wuxiaoshen@shu.edu.cn>
2019-03-11 15:13:00 +0800

    feat: benchmark

---
+++ part_five/db_example.go
@@ -0,0 +1,60 @@
+package part_five
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+type Information struct {
+	gorm.Model
+	Name string `gorm:"type:varchar" json:"name"`
+}
+
+type OtherInformation struct {
+	gorm.Model
+	Name string `gorm:"type:varchar" json:"name"`
+}
+
+var (
+	POSTGRES *gorm.DB
+)
+
+func init() {
+	host := "127.0.0.1"
+	port := "5432"
+	user := "postgres"
+	password := "root"
+	dbName := "gorm_example"
+	sslMode := "disable"
+
+	connectString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host, port, user, password, dbName, sslMode)
+	conn, err := gorm.Open("postgres", connectString)
+	if err != nil {
+		panic("failed to connect database" + err.Error())
+	}
+
+	conn.LogMode(true)
+
+	conn.DB().SetMaxIdleConns(3)
+
+	POSTGRES = conn
+	POSTGRES.AutoMigrate(&Information{}, &OtherInformation{})
+}
+
+func InsertByFirstOrInit(id uint) {
+	var infor Information
+	infor.Name = strconv.Itoa(int(id)) + " hello"
+	if dbError := POSTGRES.FirstOrInit(&infor, map[string]interface{}{"name": strconv.Itoa(int(id)) + " hello"}).Error; dbError != nil {
+		fmt.Println(dbError, id)
+	}
+	POSTGRES.Save(&infor)
+}
+
+func InsertByFirstOrCreate(id uint) {
+	var infor OtherInformation
+	POSTGRES.FirstOrCreate(&infor, OtherInformation{Name: strconv.Itoa(int(id)) + " world"})
+}
---
+++ part_five/db_example_test.go
@@ -0,0 +1,21 @@
+package part_five
+
+import "testing"
+
+func BenchmarkInsertByFirstOrInit(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		InsertByFirstOrInit(uint(i))
+	}
+}
+
+func BenchmarkInsertByFirstOrCreate(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		InsertByFirstOrCreate(uint(i))
+	}
+}
+
+func TestInsertByFirstOrInit(t *testing.T) {
+	InsertByFirstOrInit(2)
+}
